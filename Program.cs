f2cf554c938afb9c24eed6197027df957bf9300e
notbtc2 <bieloliver2006@gmail.com>
2022-12-01 11:36:03 +0100

    created Practica 2

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Practica2
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nombre = new Random().Next(1, 11);
+            int lives = 4;
+            while (lives > 0)
+            {
+                Console.WriteLine("Introdueix un nombre del 1 al 11 : ");
+                string? input = Console.ReadLine();
+                int guess;
+                if (input != null)
+                {
+                    try
+                    {
+                        guess = int.Parse(input);
+                        if (guess == nombre)
+                        {
+                            Console.WriteLine("Has guanyat");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No és correcte");
+                            --lives;
+                        }
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("No has introduit un nombre!");
+                    }
+                }
+                if (lives == 0) Console.WriteLine("Has perdut");
+            }
+        }
+    }
+}
